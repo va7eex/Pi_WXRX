83debb04910ff899a09d009bb26a4b5c3f705ab1
va7eex <david@va7eex.ca>
2017-02-04 21:53:10 -0800

    Update render.sh

--- render.sh
+++ render.sh
@@ -5,20 +5,18 @@ render_options=("ZA" "HVCT" "NO")
 
 f=$1
 
-	direction=${f:(-7):(-6)}
-	echo $f
-	f1="${f:0:(-4)}"
-	f2="${f1#'/tmp/'}"
+echo $f
+f1="${f:0:(-4)}"
+f2="${f1#'/tmp/'}"
 
-	echo $f2
-	echo Direction is $direction-bound
+echo $f2
 
-	wxmap -T "NOAA-15" -T "NOAA-18" -T "NOAA-19" -n "Spuzzum,Canada" -H ~/sat_tle.txt -o "${2}" /tmp/passmap.png
+wxmap -T "NOAA-15" -T "NOAA-18" -T "NOAA-19" -n "Spuzzum,Canada" -H ~/sat_tle.txt -o "${2}" /tmp/passmap.png
 
-	for r in "${render_options[@]}"
-	do
-		echo "Now rendering option: ${r}"
-		wxtoimg -e $r -ocK -m /tmp/passmap.png /tmp/$f2.wav /tmp/$f2"_${r}".png
+for r in "${render_options[@]}"
+do
+	echo "Now rendering option: ${r}"
+	wxtoimg -e $r -ocK -m /tmp/passmap.png /tmp/$f2.wav /tmp/$f2"_${r}".png
 
-		bash ~/ftp.sh /tmp/$f2"_${r}".png
-	done
+	bash ~/ftp.sh /tmp/$f2"_${r}".png
+done
