a0be840d81f749efbbdd4f94f8efcc88713eb3ef
va7eex <david@va7eex.ca>
2017-02-04 21:09:54 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ git clone https://github.com/nsat/pypredict
 python pypredict/setup.py install
 mkdir sat_scheds
 mkdir sat_output
+python get_tle.py
+bash sats.sh NOAA-15
+bash sats.sh NOAA-18
+bash sats.sh NOAA-19
