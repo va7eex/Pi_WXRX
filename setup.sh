bff80cafa02dd5adb30430322f75faad89c7fb65
va7eex <david@va7eex.ca>
2017-02-04 21:17:42 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install git cmake make libusb-1.0-0-dev python-dev
+apt-get install git cmake make libusb-1.0-0-dev python-dev at
 git clone https://github.com/nsat/pypredict
 python pypredict/setup.py install
 mkdir sat_scheds
